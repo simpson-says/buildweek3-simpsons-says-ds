a7d3a5a5bda9521f30bc2140c041a8f7675eb38e
derek-shing <hpshing@gmail.com>
2019-04-16 09:13:15 -0700

    update all

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ nltk.download('punkt')
 import pickle
 import gensim
 
+import os
+
+dirpath = os.getcwd()
+dirpath = dirpath +'/'
+
 
 APP = Flask(__name__)
 
@@ -14,18 +19,18 @@ t = pickle.load( open( "tf_idf.p", "rb" ) )
 s = pickle.load( open( "sims.p", "rb" ) )
 df = pickle.load( open( "df.p", "rb" ) )
 corpus = pickle.load( open( "c.p", "rb" ) )
-sims = gensim.similarities.Similarity('/',t[corpus],
-#                                      num_features=len(d))
+sims = gensim.similarities.Similarity(dirpath,t[corpus],
+num_features=len(d))
 
 query_doc = [w.lower() for w in word_tokenize("have you been to China")]
 print(query_doc)
-#query_doc_bow = d.doc2bow(query_doc)
-#query_doc_tf_idf = t[query_doc_bow]
-#v = sims[query_doc_tf_idf]
-#result = v.argsort()[-10:][::-1]
-#df['spoken_words'][result]
+query_doc_bow = d.doc2bow(query_doc)
+query_doc_tf_idf = t[query_doc_bow]
+v = sims[query_doc_tf_idf]
+result = v.argsort()[-10:][::-1]
+print(df['spoken_words'][result])
 
 
 @APP.route('/')
 def hello_world():
-    return "Hello World"
+    return df['spoken_words'][v.argmax()]
