924154e7fdb71a0ad3ef62f4af799afc26d27e78
derek-shing <hpshing@gmail.com>
2019-04-15 23:56:08 -0700

    update all

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask
 import pandas as pd
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
-nltk.download('punkt')
+#nltk.download('punkt')
 
 
 
 APP = Flask(__name__)
 df2 = pd.read_csv('/content/drive/My Drive/simpsons_script_lines.csv',error_bad_lines=False)
-token_list = list()
-for i in df2['normalized_text']:
-  token_list.append(word_tokenize(str(i)))
-df2['token'] = token_list
+#token_list = list()
+#for i in df2['normalized_text']:
+#  token_list.append(word_tokenize(str(i)))
+#df2['token'] = token_list
 
 @APP.route('/')
 def hello_world():
