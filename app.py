ff6e44329b6d0a04730cf9bc729bf2d94137b5f3
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-04-17 14:57:11 -0700

    changed 'getquote' to read from body

--- app.py
+++ app.py
@@ -13,14 +13,15 @@ t = pickle.load( open( "tf_idf.p", "rb" ) )
 s = pickle.load( open( "sims2.p", "rb" ) )
 df = pickle.load( open( "df.p", "rb" ) )
 df = df.rename(columns={'spoken_words_x':'spoken_words','raw_character_text_x':'raw_character_text'})
-# df3 = pickle.load( open( "scripts.pkl", "rb" ) )
 corpus = pickle.load( open( "c.p", "rb" ) )
+
+## Uncomment and run once for local operation
 # s = gensim.similarities.Similarity('/app/',t[corpus],num_features=len(d))
 # pickle.dump(s,open('sims2.p','wb'))
 
 
 @APP.route('/')
-@APP.route('/api',methods=['POST'])
+@APP.route('/api', methods=['POST'])
 def hello_world():
 
     user_input = "the goggles do nothing"
@@ -45,52 +46,51 @@ def hello_world():
 def getquote():
     inputs = '[1,2,3]'
     if request.method=='POST':
-        inputs = request.values['input']
-    inputs2 = [int(x) for x in inputs.strip('[]').split(',')]
-
-    condition = (df.quote_id.isin(inputs2))
+        inputs = request.get_json(force=True)['input']
+    
+    condition = (df.quote_id.isin(inputs))
     response = df[condition]
+    print(response)
     column = ['quote_id', 'raw_character_text', 'spoken_words','episode_title','season','number_in_season']
     response = response[column]
     response.to_json(orient='records')
-    print(response)
 
     return response.to_json(orient='records')
 
 
-@APP.route('/gen',methods=['POST'])
-@APP.route('/gen')
-def generator():
-    inputs = '[1,2,3]'
-    if request.method=='POST':
-        inputs = request.values['input']
+# @APP.route('/gen',methods=['POST'])
+# @APP.route('/gen')
+# def generator():
+#     inputs = '[1,2,3]'
+#     if request.method=='POST':
+#         inputs = request.values['input']
     
-    placeholder = """
-(re: gown) hmmm... the sea won't stand for this.
-i know! i'll do a rap.(beat box noise)
-(re: beaver dam) see? animals can you?
-(reading) failure to wait by you can never
-(re: car
-i need some supplies: a keg of beer(he stands and says:) after sex
-(re: car
-angle on: greeting card
-(re: clocks) look at those celebrities: of all of the sea lion.(terrified)
-and here's the greatest heroes of all the...
-i know! i'll do a rap.(beat box noise)
-(re: beaver dam) see? animals can you?
-(reading) failure to wait by you can never
-(re: car
-i need some supplies: a keg of beer(he stands and says:) after sex
-(re: car
-angle on: greeting card
-(re: clocks) look at those celebrities: of all of the sea lion.(terrified)
-and here's the greatest heroes of all the...
-i know! i'll do a rap.(beat box noise)
-(re: beaver dam) see? animals can you?
-(reading) failure to wait by you can never
-(re: car
-"""
+#     placeholder = """
+# (re: gown) hmmm... the sea won't stand for this.
+# i know! i'll do a rap.(beat box noise)
+# (re: beaver dam) see? animals can you?
+# (reading) failure to wait by you can never
+# (re: car
+# i need some supplies: a keg of beer(he stands and says:) after sex
+# (re: car
+# angle on: greeting card
+# (re: clocks) look at those celebrities: of all of the sea lion.(terrified)
+# and here's the greatest heroes of all the...
+# i know! i'll do a rap.(beat box noise)
+# (re: beaver dam) see? animals can you?
+# (reading) failure to wait by you can never
+# (re: car
+# i need some supplies: a keg of beer(he stands and says:) after sex
+# (re: car
+# angle on: greeting card
+# (re: clocks) look at those celebrities: of all of the sea lion.(terrified)
+# and here's the greatest heroes of all the...
+# i know! i'll do a rap.(beat box noise)
+# (re: beaver dam) see? animals can you?
+# (reading) failure to wait by you can never
+# (re: car
+# """
     
-    return placeholder
+#     return placeholder
 
 
